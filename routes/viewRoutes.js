426747b7d5a278310899c174342c282aa833471a
Jordan Clemens <jclemens24@hotmail.com>
2021-12-28 09:52:52 -0500

    stripe

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const viewController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -10,12 +9,7 @@ router.get('/signup', viewController.getSignUpForm);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/me', authController.verifyAuth, viewController.getAccount);
-router.get(
-  '/my-tours',
-  bookingController.createBookingCheckout,
-  authController.verifyAuth,
-  viewController.getMyTours
-);
+router.get('/my-tours', authController.verifyAuth, viewController.getMyTours);
 router.post(
   '/submit-user-data',
   authController.verifyAuth,

95b72e926566a1fd4eecedad4c5a47746ba4e602
Jordan Clemens <jclemens24@hotmail.com>
2021-12-28 11:18:53 -0500

    code refactor viewController.js

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -42,29 +42,16 @@ exports.getTour = catchAsync(async (req, res, next) => {
     );
   }
 
-  res
-    .status(200)
-    // .set(
-    //   'Content-Security-Policy',
-    //   "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://cdn.jsdelivr.net 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    // )
-    .render('tour', {
-      title: `${tour.name} Tour`,
-      tour
-    });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  });
 });
 
 exports.getLoginForm = catchAsync(async (req, res, next) => {
-  res
-    .status(200)
-    // .set(
-    //   'Content-Security-Policy',
-    //   "connect-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com https://cdn.jsdelivr.net 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
-    // )
-
-    .render('login', {
-      title: 'Log into your account'
-    });
+  res.status(200).render('login', {
+    title: 'Log into your account'
+  });
 });
 
 exports.getSignUpForm = catchAsync(async (req, res, next) => {
@@ -101,8 +88,6 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
       return await Tour.findById(el.tour);
     })
   );
-  // const tourId = bookings.map(el => el.tour);
-  // const tours = await Tour.find({ _id: { $in: tourId } });
 
   res.status(200).render('overview', {
     title: 'My Tours',

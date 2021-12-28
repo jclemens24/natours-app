bb6f0ff56c07c8876d7dbacdd15b4bbea4135d40
Jordan Clemens <jclemens24@hotmail.com>
2021-12-28 11:13:33 -0500

    changed alert dataset

--- public/js/index.js
+++ public/js/index.js
@@ -83,4 +83,4 @@ if (bookBtn) {
 }
 
 const alertMessage = document.querySelector('body').dataset.alert;
-if (alert) showAlert('success', alertMessage, 13);
+if (alertMessage) showAlert('success', alertMessage, 13);

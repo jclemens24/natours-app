f5c66db39f3fd8702df0d136bbe6bef4a6c12ac1
Jordan Clemens <jclemens24@hotmail.com>
2021-12-27 19:16:47 -0500

    Updated updateSettings.js axios baseUrl

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -8,8 +8,8 @@ export const updateUserSettings = async function (data, type) {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updatemypassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateme';
+        ? '/api/v1/users/updatemypassword'
+        : '/api/v1/users/updateme';
     const res = await axios({
       method: 'PATCH',
       url,
